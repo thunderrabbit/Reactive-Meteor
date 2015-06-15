563b5303a0ef4af6445b96068c3c5fbeb580b720
Sam Walsh <samuel.walsh@manchester.ac.uk>
2015-06-15 17:30:20 +0100

    Moved files to server and client folders

--- client/client.js
+++ client/client.js
@@ -8,54 +8,6 @@ var textArray   = ["Sam", "Sarah", "Dwight", "Bandit", "Michael"];
 var gaugeArray  = [1,90,14,56,22,100,0,150,180,90,44,200];
 var chartArray  = [0.1,0.5,0.15,0.2,0.4,0.3,0.5,0.34];
 
-/*****************************METEOR SERVER CODE******************************/
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    if (Circles.find().count() === 0) {
-      Circles.insert({data: [5,10,15,20,25,30]});
-    }
-    //Populate TextValues database with initial values
-    if (TextValues.find().count() === 0) {
-      TextValues.insert({data: textArray });
-    }
-    
-    //Populate GaugeValues database with initial values
-    if (GaugeValues.find().count() === 0) {
-      GaugeValues.insert({data: gaugeArray });
-    }
-    
-    //Populate ChartValues database with initial values
-    if (ChartValues.find().count() === 0) {
-      ChartValues.insert({data: chartArray });
-    }
-  });
-
-//This function just updates the circles and text collections every 2 seconds
-  Meteor.setInterval(function () {
-    
-    //Shuffle Circles and change collection to shuffled
-    var newCircleData = _.shuffle(Circles.findOne().data);
-    Circles.update({}, {data: newCircleData});
-    
-    //Select random array member and change collection to it
-    var randomIndex = Math.floor( Math.random() * textArray.length );
-    var element = textArray[randomIndex];
-    TextValues.update({}, {data: element});
-    
-    //Select random data value and change collection to it
-    var randomIndex2 = Math.floor( Math.random() * gaugeArray.length );
-    var dataPoint = gaugeArray[randomIndex2];
-    GaugeValues.update({}, {data: dataPoint});
-    
-    //Select random data value and change collection to it
-    var randomIndex3 = Math.floor( Math.random() * chartArray.length );
-    var dataPoint2 = chartArray[randomIndex3];
-    ChartValues.update({}, {data: dataPoint2});
-    
-  }, 1000);
-}
-
-
 /*****************************METEOR CLIENT CODE******************************/
 if (Meteor.isClient) {
   
@@ -303,14 +255,7 @@ function buildChart() {
             }]
         });
 }
-  
-          // // set up the updating of the chart each second
-          // var series = this.series[0];
-          // setInterval(function () {
-          //     var x = (new Date()).getTime(), // current time
-          //         y = Math.random();
-          //     series.addPoint([x, y], true, true);
-          // }, 1000);
+
   
 
 
---
+++ server/server.js
@@ -0,0 +1,56 @@
+//Mongodb Collections
+var Circles     = new Meteor.Collection('circles');
+var TextValues  = new Meteor.Collection('textvalues');
+var GaugeValues = new Meteor.Collection('gaugevalues');
+var ChartValues = new Meteor.Collection('chartvalues');
+
+var textArray   = ["Sam", "Sarah", "Dwight", "Bandit", "Michael"];
+var gaugeArray  = [1,90,14,56,22,100,0,150,180,90,44,200];
+var chartArray  = [0.1,0.5,0.15,0.2,0.4,0.3,0.5,0.34];
+
+/*****************************METEOR SERVER CODE******************************/
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    if (Circles.find().count() === 0) {
+      Circles.insert({data: [5,10,15,20,25,30]});
+    }
+    //Populate TextValues database with initial values
+    if (TextValues.find().count() === 0) {
+      TextValues.insert({data: textArray });
+    }
+    
+    //Populate GaugeValues database with initial values
+    if (GaugeValues.find().count() === 0) {
+      GaugeValues.insert({data: gaugeArray });
+    }
+    
+    //Populate ChartValues database with initial values
+    if (ChartValues.find().count() === 0) {
+      ChartValues.insert({data: chartArray });
+    }
+  });
+
+//This function just updates the circles and text collections every 2 seconds
+  Meteor.setInterval(function () {
+    
+    //Shuffle Circles and change collection to shuffled
+    var newCircleData = _.shuffle(Circles.findOne().data);
+    Circles.update({}, {data: newCircleData});
+    
+    //Select random array member and change collection to it
+    var randomIndex = Math.floor( Math.random() * textArray.length );
+    var element = textArray[randomIndex];
+    TextValues.update({}, {data: element});
+    
+    //Select random data value and change collection to it
+    var randomIndex2 = Math.floor( Math.random() * gaugeArray.length );
+    var dataPoint = gaugeArray[randomIndex2];
+    GaugeValues.update({}, {data: dataPoint});
+    
+    //Select random data value and change collection to it
+    var randomIndex3 = Math.floor( Math.random() * chartArray.length );
+    var dataPoint2 = chartArray[randomIndex3];
+    ChartValues.update({}, {data: dataPoint2});
+    
+  }, 1000);
+}
