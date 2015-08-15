9773943d0b335fd55d2a3d58867b8cf853492a81
Rob Nugen <rob@robnugen.com>
2015-08-15 14:23:48 +0900

    Duplicate client circles into colorful circles

--- client/client.js
+++ client/client.js
@@ -27,6 +27,11 @@ if (Meteor.isClient) {
         buildCircles();
     };
     
+    //Check Circle Template is rendered
+    Template.colorfulCirclesTemplate.rendered = function () {
+        buildColorfulCircles();
+    };
+
     //TextValues Observer
     TextValues.find().observe({
       added: function (text) {
@@ -71,6 +76,41 @@ if (Meteor.isClient) {
         series.addPoint([x, y], true, true);     
       }
     });    
+}
+
+/*
+* Function to draw colorful circles
+*/
+function buildColorfulCircles() {
+    var svg, width = 500, height = 75, x;
+
+    svg = d3.select('#colorful-circles').append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    var drawCircles = function (update) {
+      var data = Circles.findOne().data;
+      var circles = svg.selectAll('circle').data(data);
+      if (!update) {
+        circles = circles.enter().append('circle')
+          .attr('cx', function (d, i) { return x(i); })
+          .attr('cy', height / 2);
+      } else {
+        circles = circles.transition().duration(1000);
+      }
+      circles.attr('r', function (d) { return d; });
+    };
+
+   //Circles Observer
+    Circles.find().observe({
+      added: function () {
+        x = d3.scale.ordinal()
+          .domain(d3.range(Circles.findOne().data.length))
+          .rangePoints([0, width], 1);
+        drawCircles(false);
+      },
+      changed: _.partial(drawCircles, true)
+    });
 }
 
 /*
