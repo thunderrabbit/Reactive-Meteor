8454433e93800b77eead8dbee9039fa3f5e41a63
Rob Nugen <rob@robnugen.com>
2015-08-15 13:42:34 +0900

    Extracted logic for drawing circles to an external function.
    This makes client.js code style more consistent.

--- client/client.js
+++ client/client.js
@@ -24,37 +24,8 @@ if (Meteor.isClient) {
     
     //Check Circle Template is rendered
     Template.circlesTemplate.rendered = function () {
-    
-    var svg, width = 500, height = 75, x;
-
-    svg = d3.select('#circles').append('svg')
-      .attr('width', width)
-      .attr('height', height);
-
-    var drawCircles = function (update) {
-      var data = Circles.findOne().data;
-      var circles = svg.selectAll('circle').data(data);
-      if (!update) {
-        circles = circles.enter().append('circle')
-          .attr('cx', function (d, i) { return x(i); })
-          .attr('cy', height / 2);
-      } else {
-        circles = circles.transition().duration(1000);
-      }
-      circles.attr('r', function (d) { return d; });
-    };   
-
-   //Circles Observer
-    Circles.find().observe({
-      added: function () {
-        x = d3.scale.ordinal()
-          .domain(d3.range(Circles.findOne().data.length))
-          .rangePoints([0, width], 1);
-        drawCircles(false);
-      },
-      changed: _.partial(drawCircles, true)
-    });
-  };
+        buildCircles();
+    };
     
     //TextValues Observer
     TextValues.find().observe({
@@ -108,7 +79,40 @@ if (Meteor.isClient) {
     });    
 }
 
+/*
+* Function to draw the circles
+*/
+function buildCircles() {
+    var svg, width = 500, height = 75, x;
 
+    svg = d3.select('#circles').append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    var drawCircles = function (update) {
+      var data = Circles.findOne().data;
+      var circles = svg.selectAll('circle').data(data);
+      if (!update) {
+        circles = circles.enter().append('circle')
+          .attr('cx', function (d, i) { return x(i); })
+          .attr('cy', height / 2);
+      } else {
+        circles = circles.transition().duration(1000);
+      }
+      circles.attr('r', function (d) { return d; });
+    };
+
+   //Circles Observer
+    Circles.find().observe({
+      added: function () {
+        x = d3.scale.ordinal()
+          .domain(d3.range(Circles.findOne().data.length))
+          .rangePoints([0, width], 1);
+        drawCircles(false);
+      },
+      changed: _.partial(drawCircles, true)
+    });
+}
 
 /*
 * Function to draw the gauge
